88c707dfc01408271fc78c48602319791fd31c40
AlmosYT <62188528+AlmosYT@users.noreply.github.com>
2021-02-21 02:32:36 +0100

    Update questions.js

--- questions.js
+++ questions.js
@@ -2,9 +2,9 @@ questions = [
     {
         "question": "Botting is unfair to regular players and shouldn't be allowed on MVP.",
         "effect": {
-            "econ": 10,
+            "econ": -5,
             "dipl": 0,
-            "govt": 0,
+            "govt": -10,
             "scty": 0
         }
     },
@@ -20,9 +20,234 @@ questions = [
         {
         "question": "The current moderation team should be replaced.",
         "effect": {
+            "econ": 5,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+        {
+        "question": "Players should be able to place anything they want, even if it ruins the look of the canvas.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+            {
+        "question": "The moderation team of PixelPlace isn't biased.",
+        "effect": {
             "econ": 0,
             "dipl": 10,
             "govt": 0,
+            "scty": 0
+        }
+    },
+            {
+        "question": "Peace treaties are meant to be broken.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+            {
+        "question": "The current rules are too harsh.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+            {
+        "question": "Alliances shouldn't exist.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -5,
+            "scty": -5
+        }
+    },
+            {
+        "question": "You shouldn't be able to say offensive words in the chat.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+            {
+        "question": "The best state of war is none.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+            {
+        "question": "The PixelPlace Parliament is useful.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+            {
+        "question": "A global alliance, like UPF, should be created.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+            {
+        "question": "I believe I have been banned unfairly in the past.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+            {
+        "question": "We should stick to our old rules instead of making more confusing ones.",
+        "effect": {
+            "econ": -10,
+            "dipl": 00,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+            {
+        "question": "Experience is more important then liberty.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+            {
+        "question": "The current moderation team should be replaced.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+            {
+        "question": "The mods should be chosen by the mod team instead of the playerbase, because the mods are more experienced.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+            {
+        "question": "PixelPlace is a P2W game.",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+            {
+        "question": "Dotting is just another form of attacking, which shouldn't be banned.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+            {
+        "question": "A world conquest is the goal of my guild.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+            {
+        "question": "An MVP United Nations would be useless.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -5
+        }
+    },
+            {
+        "question": "Racism is acceptable in our community.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -5
+        }
+    },
+            {
+        "question": "Raids are inherently good for the community.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+            {
+        "question": "I consider myself peaceful.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+            {
+        "question": "Multi-guilding doesn't work.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+            {
+        "question": "I trust in the moderator team.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+            {
+        "question": "MVP was better when it had less players.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+            {
+        "question": "I've witnessed someone break a rule, and I didn't report them because they were my ally/friend.",
+        "effect": {
+            "econ": -5,
+            "dipl": -5,
+            "govt": -5,
             "scty": 0
         }
     }
